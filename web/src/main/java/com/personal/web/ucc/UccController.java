2010a5205c1e6d4fc9d64958f2c3ff7344dfc8e6
zhuangqianliao <zhuangqianliao@jd.com>
2018-12-19 14:38:15 +0800

    spring-ucc-client

---
+++ web/src/main/java/com/personal/web/ucc/UccController.java
@@ -0,0 +1,21 @@
+package com.personal.web.ucc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhuangqianliao
+ */
+@RestController
+@RequestMapping("/ucc")
+public class UccController {
+
+    @Autowired
+    private UccService uccService;
+
+    @RequestMapping("/test")
+    public String uccValueTest() {
+        return uccService.getJssEndpoint();
+    }
+}
--- web/src/test/java/com/personal/web/WebApplicationTests.java
+++ web/src/test/java/com/personal/web/WebApplicationTests.java
@@ -1,6 +1,5 @@
 package com.personal.web;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,10 +7,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
 }
 
