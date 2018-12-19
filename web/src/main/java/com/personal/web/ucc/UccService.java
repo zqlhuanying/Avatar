fe7cbb3d94a3c180a149692989c958fb04563ddf
zhuangqianliao <zhuangqianliao@jd.com>
2018-12-19 14:38:44 +0800

    spring-ucc-client

---
+++ web/src/main/java/com/personal/web/ucc/UccService.java
@@ -0,0 +1,16 @@
+package com.personal.web.ucc;
+
+import com.personal.service.ConfigService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zhuangqianliao
+ */
+@Service
+public class UccService {
+
+    public String getJssEndpoint() {
+        return ConfigService.getConfig("jss-config")
+                .getProperty("jss.endpoint", null);
+    }
+}
