320d1f3fd8073740d2093821a91d5039b92b2cfd
zhuangqianliao <zhuangqianliao@jd.com>
2019-02-20 11:34:25 +0800

    Snowflake

--- utils/src/main/java/com/personal/Snowflake.java
+++ utils/src/main/java/com/personal/Snowflake.java
@@ -21,13 +21,7 @@ public class Snowflake {
     private final long workerIdBits = 5L;
 
     /** 数据标识id所占的位数 */
-    private final long datacenterIdBits = 5L;
-
-    /** 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数) */
-    private final long maxWorkerId = -1L ^ (-1L << workerIdBits);
-
-    /** 支持的最大数据标识id，结果是31 */
-    private final long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
+    private final long dataCenterIdBits = 5L;
 
     /** 序列在id中占的位数 */
     private final long sequenceBits = 12L;
@@ -36,19 +30,25 @@ public class Snowflake {
     private final long workerIdShift = sequenceBits;
 
     /** 数据标识id向左移17位(12+5) */
-    private final long datacenterIdShift = sequenceBits + workerIdBits;
+    private final long dataCenterIdShift = sequenceBits + workerIdBits;
 
     /** 时间截向左移22位(5+5+12) */
-    private final long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+    private final long timestampLeftShift = sequenceBits + workerIdBits + dataCenterIdBits;
+
+    /** 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数) */
+    private final long maxWorkerId = ~(-1L << workerIdBits);
+
+    /** 支持的最大数据标识id，结果是31 */
+    private final long maxDataCenterId = ~(-1L << dataCenterIdBits);
 
     /** 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095) */
-    private final long sequenceMask = -1L ^ (-1L << sequenceBits);
+    private final long sequenceMask = ~(-1L << sequenceBits);
 
     /** 工作机器ID(0~31) */
     private long workerId;
 
     /** 数据中心ID(0~31) */
-    private long datacenterId;
+    private long dataCenterId;
 
     /** 毫秒内序列(0~4095) */
     private long sequence = 0L;
@@ -59,17 +59,17 @@ public class Snowflake {
     /**
      * 构造函数
      * @param workerId 工作ID (0~31)
-     * @param datacenterId 数据中心ID (0~31)
+     * @param dataCenterId 数据中心ID (0~31)
      */
-    public Snowflake(long workerId, long datacenterId) {
+    public Snowflake(long workerId, long dataCenterId) {
         if (workerId > maxWorkerId || workerId < 0) {
             throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", maxWorkerId));
         }
-        if (datacenterId > maxDatacenterId || datacenterId < 0) {
-            throw new IllegalArgumentException(String.format("datacenter Id can't be greater than %d or less than 0", maxDatacenterId));
+        if (dataCenterId > maxDataCenterId || dataCenterId < 0) {
+            throw new IllegalArgumentException(String.format("dataCenter Id can't be greater than %d or less than 0", maxDataCenterId));
         }
         this.workerId = workerId;
-        this.datacenterId = datacenterId;
+        this.dataCenterId = dataCenterId;
     }
 
     /**
@@ -103,7 +103,7 @@ public class Snowflake {
 
         //移位并通过或运算拼到一起组成64位的ID
         return ((timestamp - twepoch) << timestampLeftShift)
-                | (datacenterId << datacenterIdShift)
+                | (dataCenterId << dataCenterIdShift)
                 | (workerId << workerIdShift)
                 | sequence;
     }
@@ -113,7 +113,7 @@ public class Snowflake {
      * @param lastTimestamp 上次生成ID的时间截
      * @return 当前时间戳
      */
-    protected long tilNextMillis(long lastTimestamp) {
+    private long tilNextMillis(long lastTimestamp) {
         long timestamp = timeGen();
         while (timestamp <= lastTimestamp) {
             timestamp = timeGen();
@@ -125,7 +125,7 @@ public class Snowflake {
      * 返回以毫秒为单位的当前时间
      * @return 当前时间(毫秒)
      */
-    protected long timeGen() {
+    private long timeGen() {
         return System.currentTimeMillis();
     }
 }
